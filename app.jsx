102f300e17ad54706237dd9e041d06d1cbbcb298
shreeteja22 <smutukundu2206@gmail.com>
2025-05-25 15:09:19 +0530

    setup

---
+++ app.jsx
@@ -0,0 +1,7 @@
+// App.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//React.createElement => Object => HTMLRender(render)
+//JSX transpiled before reaching the js - Parcel - Babel
+//JSX => React.createElement => Object => HTMLRender(render)
