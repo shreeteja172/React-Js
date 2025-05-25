4875ef1281be193c844152e666f3d79b240bcf31
shreeteja22 <smutukundu2206@gmail.com>
2025-05-25 18:46:32 +0530

    Basic

--- app.jsx
+++ app.jsx
@@ -1,7 +1,16 @@
-// App.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 //React.createElement => Object => HTMLRender(render)
 //JSX transpiled before reaching the js - Parcel - Babel
 //JSX => React.createElement => Object => HTMLRender(render)
+// console.log("Hello")
+
+// const heading = <h1 id='heading' className='head'>Hello from JSX</h1>
+
+const Heading = () =>{
+  return <h1>Nama3te from Arrow Function</h1>;
+}
+// console.log(Heading);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Heading/>)
