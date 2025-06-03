22d0c3b65cf2c66832d964594bb35a2f92e038e7
shreeteja22 <smutukundu2206@gmail.com>
2025-06-03 20:50:53 +0530

    style : tried new routings

---
+++ src/components/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>
+        <h1>Well Say my name</h1>
+        <h2>To Contact me go to contact page</h2>
+    </div>
+  )
+}
+
+export default User
